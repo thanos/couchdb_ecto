ff2858e321515faf72ea3c3f40fb0271db4a83b1
thanos <thanosv@gmail.com>
2018-11-03 21:16:38 -0400

    updated mix

--- mix.exs
+++ mix.exs
@@ -6,9 +6,11 @@ defmodule CouchdbEcto.MixProject do
       app: :couchdb_ecto,
       version: "0.1.0",
       elixir: "~> 1.6",
-      start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      test_coverage: [tool: Coverex.Task],
       deps: deps()
-    ]
+    ] 
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -21,7 +23,12 @@ defmodule CouchdbEcto.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:ecto, "~> 3.0"},
+      {:couchbeam_amuino, "~> 1.4.3-amuino.8"},
+      {:mix_test_watch, "~> 0.2.6", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.7.2", only: [:dev, :test], runtime: false},
+      {:coverex, "~> 1.4.10", only: :test}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
